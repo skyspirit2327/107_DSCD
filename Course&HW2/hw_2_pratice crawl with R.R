2e8f646915ae19b1f2f1c01071d7686090731af3
skyspirit2327 <skyspirit2327@yahoo.com.tw>
2018-03-15 00:00:12 +0800

    new ver. 1.0

--- Course&HW2/hw_2_pratice crawl with R.R
+++ Course&HW2/hw_2_pratice crawl with R.R
@@ -3,24 +3,18 @@ library(rvest)
 library(magrittr)
 #找到1~3頁全部的標題
 for(i in 1:3){
-  sc=paste('https://forum.gamer.com.tw/B.php?page=',i,'&bsn=3037',sep='')
-  read_html(sc)
-  title=read_html(sc) 
-  title=html_nodes(title,".b-list__main")
-  title=html_text(title)
+  sc.url=paste('https://forum.gamer.com.tw/B.php?page=',i,'&bsn=3037',sep='')
+  title=read_html(sc.url) %>% html_nodes(".b-list__main") %>% html_text()
   title=gsub("\n", "", title)
-  cat(title,sep='\n')
+  print(paste(title,sep='\n'))
 }
 #找到1~10頁有關蟲族的標題
 for(i in 1:10){
-  sc=paste('https://forum.gamer.com.tw/B.php?page=',i,'&bsn=3037',sep='')
-  read_html(sc)
-  title=read_html(sc) 
-  title=html_nodes(title,".b-list__main")
-  title=html_text(title)
+  sc.url=paste('https://forum.gamer.com.tw/B.php?page=',i,'&bsn=3037',sep='')
+  title=read_html(sc.url) %>% html_nodes(".b-list__main") %>% html_text()
   title=gsub("\n", "", title)
   title=title[grepl("蟲族",title)==TRUE ]
-  cat(title,sep='\n')
+  print(paste(title,sep='\n'))
 }
 
 
