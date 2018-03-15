c7067c09a5084a03e5345c2107487858dfbe9786
skyspirit2327 <skyspirit2327@yahoo.com.tw>
2018-03-15 12:30:58 +0800

    ver 2.0

--- Course&HW2/hw_2_pratice crawl with R.R
+++ Course&HW2/hw_2_pratice crawl with R.R
@@ -2,19 +2,20 @@ library(xml2)
 library(rvest)
 library(magrittr)
 #找到1~3頁全部的標題
+result=c()
 for(i in 1:3){
   sc.url=paste('https://forum.gamer.com.tw/B.php?page=',i,'&bsn=3037',sep='')
   title=read_html(sc.url) %>% html_nodes(".b-list__main") %>% html_text()
   title=gsub("\n", "", title)
-  print(paste(title,sep='\n'))
+  result[[i]]=title
 }
+unlist(result)
 #找到1~10頁有關蟲族的標題
 for(i in 1:10){
   sc.url=paste('https://forum.gamer.com.tw/B.php?page=',i,'&bsn=3037',sep='')
   title=read_html(sc.url) %>% html_nodes(".b-list__main") %>% html_text()
   title=gsub("\n", "", title)
   title=title[grepl("蟲族",title)==TRUE ]
-  print(paste(title,sep='\n'))
+  result[[i]]=title
 }
-
-
+unlist(result)
