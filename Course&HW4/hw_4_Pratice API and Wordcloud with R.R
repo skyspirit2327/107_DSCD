63f9c4a6b7a30d1ef0ae6ebaadb6ff646516612c
skyspirit2327 <skyspirit2327@yahoo.com.tw>
2018-03-29 12:02:27 +0800

    ver.1.0

---
+++ Course&HW4/hw_4_Pratice API and Wordcloud with R.R
@@ -0,0 +1,35 @@
+library(httr)
+token = ''
+urlhead ='https://graph.facebook.com/v2.12/StarCraftZHTW/posts?fields=message&access_token='
+url = paste0(urlhead,token)
+res = httr::GET(url)
+title = httr::content(res)
+title=title$data
+title=unlist(title)
+direction='D:/SkySpiriT/NTU/107_DSCD/hw4/corpus.txt'
+write(title, file =direction,
+      ncolumns = length(title), sep=",")
+
+library(rJava)
+library(tm) 
+library(Rwordseg) 
+library(tmcn) 
+library(wordcloud) 
+library(colorspace)
+Data = Corpus(VectorSource(title))
+Data = tm_map(Data ,stripWhitespace)
+Data = tm_map(Data ,removePunctuation)
+Data = tm_map(Data ,removeNumbers)
+Data = tm_map(Data ,function(word){gsub("[A-Za-z0-9]","", word)})
+stopward=stopwordsCN()
+Data = tm_map(Data,removeWords,stopward)
+
+Wcloud = segmentCN(Data$content, nature = TRUE)
+Wcloud = unlist( Wcloud )
+noun = Wcloud[ names ( Wcloud ) == "n" ]
+tab = table( noun )
+Word = as.data.frame( tab[ tab >= 1 ] )
+
+wordcloud(words = Word$noun, freq = Word$Freq,min.freq =2,
+  random.order = F, ordered.colors = T, scale=c(3,0.1),
+  colors = rainbow_hcl( nrow( Word ) ))
